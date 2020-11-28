027bb86ba15524ec3c0b8ea72baab2bfdb76b46d
Dongeun Paeng <dongeun.paeng@gmail.com>
2020-11-28 20:18:20 +0900

    Separating test environment...

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDk-MXKjHmeluikYKCIvnTLlO2lgkTm05o",
-  authDomain: "expensify-137b4.firebaseapp.com",
-  databaseURL: "https://expensify-137b4.firebaseio.com",
-  projectId: "expensify-137b4",
-  storageBucket: "expensify-137b4.appspot.com",
-  messagingSenderId: "716318864073",
-  appId: "1:716318864073:web:4daa11c287b2dcbd669318",
-  measurementId: "G-JH8VZYN9MS"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
-module.exports = env => {
+dotenv.config();
+console.log(process.env.FIREBASE_API_KEY);
+
+module.exports = (env) => {
   const isProduction = env === "production";
 
   return {
     entry: "./src/app.js",
     output: {
       path: path.join(__dirname, "public", "dist"),
-      filename: "bundle.js"
+      filename: "bundle.js",
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new webpack.EnvironmentPlugin([
+        "FIREBASE_API_KEY",
+        "FIREBASE_AUTH_DOMAIN",
+        "FIREBASE_DATABASE_URL",
+        "FIREBASE_PROJECT_ID",
+        "FIREBASE_STORAGE_BUCKET",
+        "FIREBASE_MESSAGING_SENDER_ID",
+        "FIREBASE_APP_ID",
+        "FIREBASE_MEASUREMENT_ID",
+      ]),
+    ],
     module: {
       rules: [
         {
           loader: "babel-loader",
           test: /\.js$/,
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.s?css$/,
@@ -24,16 +41,16 @@ module.exports = env => {
             MiniCssExtractPlugin.loader,
             // "style-loader",
             "css-loader",
-            "sass-loader"
-          ]
-        }
-      ]
+            "sass-loader",
+          ],
+        },
+      ],
     },
     devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
-      publicPath: '/dist/'
-    }
+      publicPath: "/dist/",
+    },
   };
 };
